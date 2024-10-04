3ca58993acde2645ae40ece686b777cb58b9aba7
Christian Remboldt <chris.remboldt.cr@gmail.com>
2024-10-04 15:29:55 +0200

    bug  fix

--- ipfslib/IPFS/resolve.py
+++ ipfslib/IPFS/resolve.py
@@ -3,7 +3,10 @@ import requests
 
 # Resolve IPNS name
 def resolve(api, ipns_name: str) -> None:
-    response = requests.post('http://{endpoint}/api/v0/name/resolve'.format(endpoint=api.endpoint), params=ipns_name)
+    params = {
+        'arg': ipns_name
+    }
+    response = requests.post('http://{endpoint}/api/v0/name/resolve'.format(endpoint=api.endpoint), params=params)
     raw_json = response.text
     try:
         return json.loads(raw_json)['Path']
