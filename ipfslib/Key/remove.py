9a8eb4731dc13a962de96baf96517c3e6a89e647
Christian Remboldt <chris.remboldt.cr@gmail.com>
2024-10-04 18:22:20 +0200

    removed debug code

--- ipfslib/Key/remove.py
+++ ipfslib/Key/remove.py
@@ -10,7 +10,7 @@ def remove(api, key_name: str, extra_info: bool = False) -> List[dict]:
     }
     response = requests.post('http://{endpoint}/api/v0/key/rm'.format(endpoint=api.endpoint), params=params)
     raw_json = response.text
-    print(raw_json)
+    
     try:
         return json.loads(raw_json)['Keys']
     except KeyError:
